8981160b3a8d68e42cf8d39acdf80f649ade97fb
Amela Bundic <60113752+amelabundic@users.noreply.github.com>
2020-09-21 10:44:56 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using aspMvcDatabaseFirst1.Models;
+
+namespace aspMvcDatabaseFirst1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly Osobadb2109Context db;
+
+        public HomeController(Osobadb2109Context _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Osoba.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SQLQuery2109.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Osoba(
+OsobaId int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+Ime nvarchar(50) NOT NULL,
+Prezime nvarchar(30) NOT NULL,
+Adresa nvarchar(30) NOT NULL,
+DatumRodjenja date NOT NULL,
+Telefon varchar(20) NOT NULL
+)
